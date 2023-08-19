495f423fef35f10ac4d38ce449251e75d10ef5fe
Pranav <pranavkp.me@outlook.com>
2023-08-19 16:47:51 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Pranav Person website
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import ThemeSwitch from "@/components/utils/ThemeSwitch";
+
+export default function Home() {
+  return (
+    <main className="h-screen">
+      <ThemeSwitch />
+    </main>
+  );
+}
