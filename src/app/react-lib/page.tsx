e25a09f7f10a207ddb7700b785235c7a1edb159b
Pranav <pranavkp.me@outlook.com>
2023-08-21 20:22:54 +0530

    react lib page added

---
+++ src/app/react-lib/page.tsx
@@ -0,0 +1,98 @@
+import { Container } from "@/components/core/core.container";
+import { IconTitleCard } from "@/components/core/core.iconTitleCard";
+import { Section } from "@/components/core/core.section";
+import { Icon } from "@/components/utils/utils.icon";
+import {
+  DocumentationData,
+  CssInJsData,
+  FetchersData,
+  FrameworkData,
+  NotesData,
+  ReactDataFetchingData,
+  StateManagementData,
+  UiLibraryData,
+  UtilityData,
+} from "@/data/data.reactLib";
+import Image from "next/image";
+import { FC } from "react";
+
+const ReactLib = () => {
+  return (
+    <Container
+      title="Frontend Libraries"
+      description="Frontend echo system to help you build your frontend website faster"
+    >
+      {ListOfLibraries.map((item, index) => (
+        <Section title={item.title} key={item.title}>
+          <IconTitleCard>
+            <div className="grid grid-cols-2 gap-6 mb-8">
+              {item.list.map((data) => (
+                <LibraryCard {...data} key={data.name} />
+              ))}
+            </div>
+          </IconTitleCard>
+        </Section>
+      ))}
+    </Container>
+  );
+};
+
+export default ReactLib;
+
+const ListOfLibraries = [
+  { title: `CSS in JS`, list: CssInJsData },
+  { title: `Document Each Component`, list: DocumentationData },
+  { title: `Fetchers`, list: FetchersData },
+  { title: `Framework`, list: FrameworkData },
+  { title: `Note It down`, list: NotesData },
+  { title: `Data Fetching Hooks`, list: ReactDataFetchingData },
+  { title: `State Management`, list: StateManagementData },
+  { title: `UI Library`, list: UiLibraryData },
+  { title: `Utility`, list: UtilityData },
+];
+
+const LibraryCard: FC<{
+  githubLink: string;
+  name: string;
+  path: string;
+  tried: boolean;
+  website?: string;
+}> = ({ githubLink, name, path, website }) => {
+  return (
+    <a
+      href={website || githubLink}
+      target="_blank"
+      className="group relative cursor-pointer"
+    >
+      <div className="p-2 border-4 border-zinc-100 dark:border-zinc-900 rounded-2xl w-full relative h-16 flex items-center justify-between dark:hover:bg-zinc-900 hover:bg-zinc-100">
+        <div className="flex items-center space-x-4">
+          {path ? (
+            <Image src={`/${path}`} width={40} height={40} alt={name} />
+          ) : (
+            <div className="w-10 h-10 rounded-full flex justify-center items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                />
+              </svg>
+            </div>
+          )}
+          <p className="font-medium">{name}</p>
+        </div>
+        <Icon
+          name="external-link"
+          className="w-5 h-5 group-hover:block hidden"
+        />
+      </div>
+    </a>
+  );
+};
---
+++ src/data/data.reactLib.ts
@@ -0,0 +1,268 @@
+const CssInJsData = [
+  {
+    githubLink: `https://github.com/emotion-js/emotion`,
+    website: `https://emotion.sh/docs/introduction`,
+    name: `Emotion`,
+    path: `lib-icons/emotion.png`,
+    tried: true,
+  },
+  {
+    githubLink: `https://github.com/styled-components/styled-components`,
+    website: `https://styled-components.com/`,
+    name: `Styled Components`,
+    path: `lib-icons/styled-components.png`,
+    tried: true,
+  },
+  {
+    githubLink: `https://github.com/cssinjs/jss`,
+    website: `https://cssinjs.org/?v=v10.3.0`,
+    name: `JSS`,
+    path: `lib-icons/jss.png`,
+    tried: false,
+  },
+  {
+    githubLink: `https://github.com/paypal/glamorous`,
+    website: `https://github.com/paypal/glamorous#readme`,
+    name: `Glamorous`,
+    path: `lib-icons/glamorous.jpg`,
+    tried: false,
+  },
+];
+const DocumentationData = [
+  {
+    githubLink: `https://github.com/facebook/docusaurus`,
+    website: `https://v2.docusaurus.io/`,
+    name: `Docusaurus`,
+    path: `lib-icons/docusaurus.png`,
+    tried: true,
+  },
+  {
+    githubLink: `https://github.com/doczjs/docz`,
+    website: `https://www.docz.site/`,
+    name: `Docz`,
+    path: `lib-icons/docz.png`,
+    tried: true,
+  },
+  {
+    githubLink: `https://github.com/storybookjs/storybook`,
+    website: `https://storybook.js.org/`,
+    name: `Story Book`,
+    path: `lib-icons/storyBook.png`,
+    tried: false,
+  },
+  {
+    githubLink: `https://github.com/react-cosmos/react-cosmos`,
+    website: `https://reactcosmos.org/`,
+    name: `React cosmos`,
+    path: `lib-icons/reactcosmos.png`,
+    tried: false,
+  },
+];
+const FetchersData = [
+  {
+    githubLink: `https://github.com/axios/axios`,
+    name: `Axios`,
+    path: ``,
+    tried: true,
+  },
+  {
+    githubLink: `https://github.com/matthew-andrews/isomorphic-fetch`,
+    name: `Isomorphic Fetch`,
+    path: ``,
+    tried: true,
+  },
+];
+const FrameworkData = [
+  {
+    githubLink: `https://github.com/facebook/react/`,
+    website: `https://reactjs.org/`,
+    name: `React`,
+    path: `lib-icons/react.png`,
+    tried: true,
+  },
+  {
+    githubLink: `https://github.com/gatsbyjs/gatsby`,
+    website: `https://www.gatsbyjs.org/`,
+    name: `Gatsby`,
+    path: `lib-icons/gatsby.png`,
+    tried: true,
+  },
+  {
+    githubLink: `https://github.com/vercel/next.js`,
+    website: `https://nextjs.org/`,
+    name: `Next`,
+    path: `lib-icons/nextjs.svg`,
+    tried: true,
+  },
+];
+const NotesData = [
+  {
+    githubLink: `https://github.com/docsifyjs/docsify/`,
+    website: `https://docsify.js.org/#/`,
+    name: `Docsify`,
+    path: `lib-icons/docsify.png`,
+    tried: true,
+  },
+];
+
+const ReactDataFetchingData = [
+  {
+    githubLink: `https://github.com/vercel/swr`,
+    website: `https://swr.vercel.app/`,
+    name: `SWR`,
+    path: `lib-icons/swr.png`,
+    tried: true,
+  },
+  {
+    githubLink: `https://github.com/tannerlinsley/react-query`,
+    website: `https://react-query.tanstack.com/`,
+    name: `React Query`,
+    path: `lib-icons/react-query.svg`,
+    tried: false,
+  },
+  {
+    githubLink: `https://github.com/coinbase/rest-hooks`,
+    website: `https://resthooks.io/`,
+    name: `Rest Hooks`,
+    path: `lib-icons/rest_hooks_logo.png`,
+    tried: false,
+  },
+  {
+    githubLink: `https://github.com/coinbase/rest-hooks`,
+    website: `https://docs.react-async.com/`,
+    name: `React Async`,
+    path: `lib-icons/react-async.png`,
+    tried: false,
+  },
+];
+
+const StateManagementData = [
+  {
+    githubLink: `https://github.com/reduxjs/react-redux`,
+    website: `https://react-redux.js.org/`,
+    name: `Redux`,
+    path: `lib-icons/redux.png`,
+    tried: true,
+  },
+  {
+    githubLink: `https://github.com/mobxjs/mobx`,
+    website: `https://mobx.js.org/README.html`,
+    name: `MobX`,
+    path: `lib-icons/mobx.png`,
+    tried: false,
+  },
+  {
+    githubLink: `https://github.com/pmndrs/zustand`,
+    website: `https://zustand.surge.sh/`,
+    name: `Zustand`,
+    path: `lib-icons/zustand.png`,
+    tried: true,
+  },
+];
+const UiLibraryData = [
+  {
+    githubLink: `https://github.com/chakra-ui/chakra-ui`,
+    website: `https://chakra-ui.com/`,
+    name: `Chakra UI`,
+    path: `lib-icons/chakra.png`,
+    tried: true,
+  },
+  {
+    githubLink: `https://github.com/ant-design/ant-design/`,
+    website: `https://ant.design/`,
+    name: `Ant Design`,
+    path: `lib-icons/ant.svg`,
+    tried: true,
+  },
+  {
+    githubLink: `https://github.com/mui-org/material-ui`,
+    website: `https://material-ui.com/`,
+    name: `Material UI`,
+    path: `lib-icons/material.png`,
+    tried: true,
+  },
+  {
+    githubLink: `https://github.com/mui-org/material-ui`,
+    website: `https://react-bootstrap.github.io/`,
+    name: `Bootstrap`,
+    path: `lib-icons/reactbootstrap.jpeg`,
+    tried: true,
+  },
+  {
+    githubLink: `https://github.com/grommet/grommet`,
+    website: `https://v2.grommet.io/`,
+    name: `Grommet`,
+    path: `lib-icons/grommet.png`,
+    tried: false,
+  },
+  {
+    githubLink: `https://github.com/rebassjs/rebass`,
+    website: `https://rebassjs.org/`,
+    name: `Rebass`,
+    path: `lib-icons/rebass.png`,
+    tried: false,
+  },
+  {
+    githubLink: `https://github.com/Semantic-Org/Semantic-UI-React`,
+    website: `https://react.semantic-ui.com/`,
+    name: `Semantic`,
+    path: `lib-icons/semantic.png`,
+    tried: false,
+  },
+  {
+    githubLink: `https://github.com/palantir/blueprint`,
+    website: `https://blueprintjs.com/`,
+    name: `Blueprint`,
+    path: `lib-icons/blueprintjs.png`,
+    tried: false,
+  },
+  {
+    githubLink: `https://github.com/segmentio/evergreen`,
+    website: `https://evergreen.segment.com/`,
+    name: `Evergreen`,
+    path: `lib-icons/evergreen.jpeg`,
+    tried: false,
+  },
+];
+const UtilityData = [
+  {
+    githubLink: `https://github.com/linkstrifer/react-rough-notation`,
+    website: `https://roughnotation.com/`,
+    name: `Rough Notation`,
+    path: `lib-icons/react-rough-notation.png`,
+    tried: true,
+  },
+  {
+    githubLink: `https://github.com/elrumordelaluz/reactour`,
+    website: `https://reactour.js.org/`,
+    name: `Reactour`,
+    path: `lib-icons/reactour.svg`,
+    tried: false,
+  },
+  {
+    githubLink: `https://github.com/react-dropzone/react-dropzone`,
+    website: `https://react-dropzone.js.org/`,
+    name: `React dropzone`,
+    path: `lib-icons/react-dropzone.png`,
+    tried: false,
+  },
+  {
+    githubLink: `https://github.com/atlassian/react-beautiful-dnd`,
+    website: `https://react-beautiful-dnd.netlify.app/`,
+    name: `React beautiful dnd`,
+    path: `lib-icons/react-beautiful-dnd.png`,
+    tried: true,
+  },
+];
+
+export {
+  CssInJsData,
+  DocumentationData,
+  FetchersData,
+  FrameworkData,
+  NotesData,
+  ReactDataFetchingData,
+  StateManagementData,
+  UiLibraryData,
+  UtilityData,
+};
