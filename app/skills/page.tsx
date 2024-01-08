996fc754d44e7f5e3718e2dae57ce598f5b41d44
Pranav <pranavkp.me@outlook.com>
2024-01-08 21:52:49 +0530

    (chore) updated the SEO for skills page

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -10,12 +10,44 @@ import {
   UtilityData,
 } from "@/data/skillData";
 import SkillCard from "./component/skillCard";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  alternates: {
+    canonical: "/skills",
+    languages: {
+      "en-US": "/en-US",
+    },
+  },
+  title:
+    "Tech-Savvy Professional: A Journey of Expertise in Frameworks, Languages, and Methodologies | Pranav P",
+  description:
+    "Embark on a voyage through my professional evolution, where I've cultivated a wealth of technology expertise. Dive into the depths of my hands-on experience, as I hone skills in diverse frameworks, languages, and methodologies. This journey not only elevates my software development proficiency but also enriches problem-solving capabilities and widens my tech perspective. Join me in navigating the dynamic tech landscape, fueled by a commitment to continuous learning and adaptability",
+  keywords: [
+    "Technology",
+    "JavaScript",
+    "React",
+    "UI/UX",
+    "Portfolio",
+    "Skills",
+  ],
+  robots: "index, follow",
+  openGraph: {
+    title:
+      "Tech-Savvy Professional: A Journey of Expertise in Frameworks, Languages, and Methodologies | Pranav P",
+    description:
+      "Embark on a voyage through my professional evolution, where I've cultivated a wealth of technology expertise. Dive into the depths of my hands-on experience, as I hone skills in diverse frameworks, languages, and methodologies. This journey not only elevates my software development proficiency but also enriches problem-solving capabilities and widens my tech perspective. Join me in navigating the dynamic tech landscape, fueled by a commitment to continuous learning and adaptability",
+    url: `${process.env.PUBLIC_URL}/skills`,
+    type: "website",
+    siteName: " Skills | Pranav P",
+  },
+};
 
 const Page = () => {
   return (
     <main className="max-w-2xl w-full m-auto mb-36">
       <header className="mt-16">
-        <h1 className="text-xl">Skills & technology </h1>
+        <h1 className="text-xl">Skills & technology</h1>
         <p className="mt-3 text-primary-white-300 text-pretty font-mono text-sm">
           Throughout my professional journey, I&apos;ve gained extensive
           technology expertise, honing skills in frameworks, languages, and
