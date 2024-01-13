f0a6d145d109e0563f1cdd1830d76e334af38991
Pranav <pranavkp.me@outlook.com>
2024-01-13 12:37:21 +0530

    (feat)Updated about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,39 @@
 const Page = () => {
-  return <div>About</div>;
+  return (
+    <main className="max-w-2xl w-full m-auto ">
+      <header className="mt-16 flex flex-col text-pretty font-mono gap-6 text-primary-white-300">
+        <h1>
+          Hey, I&apos;m Pranav, a passionate software engineer with a love for
+          gadgets and design. I vividly recall the excitement when smartphones
+          were taking off – that&apos;s when I got my first one at 17. Similar
+          to the rapid evolution of those devices, we&apos;re now witnessing
+          exciting advancements in web3 and AI.
+        </h1>
+
+        <h2>
+          As a curious kid, I always asked &apos;why.&apos; That curiosity
+          steered me into the world of software. I find joy in writing blogs,
+          crafting cool websites, and building SAAS products to grasp what users
+          think.
+        </h2>
+
+        <h3>
+          In my downtime, you&apos;ll catch me on the volleyball court,
+          immersing myself in spiritual content, and exploring the vast
+          landscapes of video games. Life, to me, is an adventure, and I&apos;m
+          all in! 🌟🚀
+        </h3>
+
+        <h4>
+          Lastly, let&apos;s talk about the power of thought. Life&apos;s
+          difficulty often reflects our mindset. Stay positive, tackle
+          challenges, but never let it turn you cruel. Be humble, open-minded,
+          and focus on finding solutions rather than adding more problems.
+          Life&apos;s journey is about growth and kindness. 🌈✨
+        </h4>
+      </header>
+    </main>
+  );
 };
 
 export default Page;
