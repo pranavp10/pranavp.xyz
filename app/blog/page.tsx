6e87bfc86baea46f24ee875f0898c2a268fc1ba1
Pranav <pranavkp.me@outlook.com>
2024-02-09 09:52:48 +0530

    updated the description

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -10,10 +10,11 @@ const Page = async () => {
       <header className="mt-16">
         <h1 className="text-xl font-medium">Blog</h1>
         <p className="mt-6 text-zinc-500 dark:text-dark-white-300 text-pretty font-mono text-sm">
-          I love writing blogs it&apos;s mostly about tech, success and
-          sprituality but mostly tech and it&apos;s always about the common
-          problem that we face duing software engineering how to face failer and
-          how to solve a problem
+          I have a passion for blogging, primarily focusing on topics such as
+          technology, success, and spirituality. However, my main emphasis is on
+          technology, and my content consistently addresses the common
+          challenges encountered in software engineering. I delve into subjects
+          like overcoming failure and problem-solving strategies.
         </p>
         <div className="mt-10 grid sm:grid-cols-2 gap-5">
           {blogs.map((blogDetails: BlogType | null) =>
