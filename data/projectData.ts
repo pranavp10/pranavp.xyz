20d2facfd3962ebcf931a43d6ba7be87f976f3c3
Pranav <pranavkp.me@outlook.com>
2024-04-22 20:57:26 +0530

    added new tool

--- data/projectData.ts
+++ data/projectData.ts
@@ -1,19 +1,27 @@
 export const projects = [
     {
+        link: "https://invoice-generator.prolab.sh/",
+        displayLink: "invoice-generator.prolab.sh",
+        description: "Tool for freelancers, small businesses, and contractors",
+        githubLink: "https://github.com/pranavp10/invoice-generator",
+        title: "Invoice Generator",
+        imageUrl: "/logo/invoice-generator.png"
+    },
+    {
+        link: "https://videocompress.prolab.sh/",
+        displayLink: "videocompress.prolab.sh",
+        description: "Compress videos for free and forever.",
+        githubLink: "https://github.com/pranavp10/video-compress",
+        title: "Video Compress",
+        imageUrl: "/logo/video-compress-prolab-sh.svg"
+    },
+    {
         link: "https://dayblog.app",
         displayLink: "dayblog.app",
         description: "The more beautiful way to journal your day and summarize with AI",
         githubLink: "https://github.com/pranavp10/ai-journal",
         title: "AI Journal",
         imageUrl: "/logo/ai-dayblog.svg"
-    },
-    {
-        link: "https://videocompress.prolab.sh/",
-        displayLink: "videocompress.prolab.sh",
-        description: "Compress Videos for Free. Forever.",
-        githubLink: "https://github.com/pranavp10/video-compress",
-        title: "Video Compress",
-        imageUrl: "/logo/video-compress-prolab-sh.svg"
     },
     {
         link: "http://www.aidetective.xyz/",
