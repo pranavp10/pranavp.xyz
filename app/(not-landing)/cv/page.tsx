0198451032e75674bc8a89de17981caba1fe3f1a
Pranav <pranavkp.me@outlook.com>
2024-05-13 17:46:55 +0530

    added padding to cv

--- app/(not-landing)/cv/page.tsx
+++ app/(not-landing)/cv/page.tsx
@@ -32,145 +32,144 @@ export const metadata: Metadata = {
   },
 };
 
-const Page = () => {
-  return (
-    <main className="max-w-2xl w-full m-auto pb-20">
-      <div className="flex mt-16 gap-5 items-center">
-        <header>
-          <h1 className="font-semibold text-2xl">{name}</h1>
-          <p className="text-zinc-500 dark:text-dark-white-300 mt-2 tracking-wider">
-            {summary}
-          </p>
-        </header>
+const Page = () => (
+  <main className="max-w-2xl w-full m-auto pb-20 p-4">
+    <div className="flex mt-16 gap-5 items-center">
+      <header>
+        <h1 className="font-semibold text-2xl">{name}</h1>
+        <p className="text-zinc-500 dark:text-dark-white-300 mt-2 tracking-wider">
+          {summary}
+        </p>
+      </header>
+    </div>
+    <div>
+      <h2 className="mt-8 mb-5 text-lg">Contact</h2>
+      <div className="grid grid-cols-4 gap-y-4">
+        {contact.social.map(({ url, name, displayName }) => (
+          <>
+            <p className="text-zinc-500 dark:text-dark-white-300" key={url}>
+              {name}
+            </p>
+            <a
+              target="_blank"
+              rel="rel"
+              href={url}
+              key={url}
+              className="col-span-3 flex items-center gap-1 hover:text-highlight group"
+            >
+              {displayName}
+              <Icon
+                name="social-link"
+                className="w-1.5 h-1.5 group-hover:scale-125"
+              />
+            </a>
+          </>
+        ))}
       </div>
-      <div>
-        <h2 className="mt-8 mb-5 text-lg">Contact</h2>
-        <div className="grid grid-cols-4 gap-y-4">
-          {contact.social.map(({ url, name, displayName }) => (
-            <>
-              <p className="text-zinc-500 dark:text-dark-white-300" key={url}>
-                {name}
-              </p>
-              <a
-                target="_blank"
-                rel="rel"
-                href={url}
-                key={url}
-                className="col-span-3 flex items-center gap-1 hover:text-highlight group"
-              >
-                {displayName}
-                <Icon
-                  name="social-link"
-                  className="w-1.5 h-1.5 group-hover:scale-125"
+    </div>
+    <div className="mt-8">
+      <h3 className="mb-5 text-lg">Work Experience</h3>
+      <div className="mt-3 grid gap-5">
+        {work.map((data) => (
+          <CVExperienceCard
+            link={data.link}
+            title={data.title}
+            descriptions={data.descriptions}
+            key={data.company}
+            location={data.location}
+            company={data.company}
+            start={data.start}
+            end={data.end}
+          />
+        ))}
+      </div>
+      <div className="mt-8">
+        <h3 className="mb-5 text-lg">Projects</h3>
+        <div className="space-y-10">
+          {projects.map((project) => (
+            <div key={project.title} className="grid grid-cols-4 gap-y-4">
+              <p className="text-zinc-500 dark:text-dark-white-300">2024</p>
+              <div className="col-span-3 space-y-2 transition ease-in-out duration-200  cursor-pointer p-4 border dark:border-dark-white-50 rounded-xl dark:hover:border-dark-white-100 flex flex-col hover:border-highlight group relative">
+                <a
+                  target="_blank"
+                  href={project.link}
+                  className="group-hover:text-highlight group flex gap-1 absolute inset-0"
                 />
-              </a>
-            </>
+                <Image
+                  src={project.imageUrl}
+                  width={36}
+                  height={36}
+                  className="w-9 h-9 rounded-lg"
+                  alt="logo of project"
+                />
+                <div>
+                  <a
+                    target="_blank"
+                    href={project.link}
+                    className="group-hover:text-highlight group flex gap-1"
+                  >
+                    <p>{project.title}</p>
+                    <Icon
+                      name="social-link"
+                      className="w-2 h-2 group-hover:scale-125 mt-1.5"
+                    />
+                  </a>
+                  <p className="text-zinc-500 dark:text-dark-white-300 tracking-wider text-sm text-pretty mt-1">
+                    {project.description}
+                  </p>
+                </div>
+              </div>
+            </div>
           ))}
         </div>
       </div>
       <div className="mt-8">
-        <h3 className="mb-5 text-lg">Work Experience</h3>
-        <div className="mt-3 grid gap-5">
-          {work.map((data) => (
-            <CVExperienceCard
-              link={data.link}
-              title={data.title}
-              descriptions={data.descriptions}
-              key={data.company}
-              location={data.location}
-              company={data.company}
-              start={data.start}
-              end={data.end}
-            />
-          ))}
-        </div>
-        <div className="mt-8">
-          <h3 className="mb-5 text-lg">Projects</h3>
-          <div className="space-y-10">
-            {projects.map((project) => (
-              <div key={project.title} className="grid grid-cols-4 gap-y-4">
-                <p className="text-zinc-500 dark:text-dark-white-300">2024</p>
-                <div className="col-span-3 space-y-2 transition ease-in-out duration-200  cursor-pointer p-4 border dark:border-dark-white-50 rounded-xl dark:hover:border-dark-white-100 flex flex-col hover:border-highlight group relative">
-                  <a
-                    target="_blank"
-                    href={project.link}
-                    className="group-hover:text-highlight group flex gap-1 absolute inset-0"
-                  />
-                  <Image
-                    src={project.imageUrl}
-                    width={36}
-                    height={36}
-                    className="w-9 h-9 rounded-lg"
-                    alt="logo of project"
-                  />
-                  <div>
-                    <a
-                      target="_blank"
-                      href={project.link}
-                      className="group-hover:text-highlight group flex gap-1"
-                    >
-                      <p>{project.title}</p>
-                      <Icon
-                        name="social-link"
-                        className="w-2 h-2 group-hover:scale-125 mt-1.5"
-                      />
-                    </a>
-                    <p className="text-zinc-500 dark:text-dark-white-300 tracking-wider text-sm text-pretty mt-1">
-                      {project.description}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="mt-8">
-          <h3 className="mb-5 text-lg">Skills</h3>
-          <div className="grid grid-cols-4 gap-y-4">
-            <p className="text-zinc-500 dark:text-dark-white-300">Tech Stack</p>
-            <div className="col-span-3">
-              <div className="flex gap-2 flex-wrap">
-                {skills.map((skill) => (
-                  <p
-                    key={skill}
-                    className="dark:bg-dark-white-100 py-0.5 text-sm px-3 rounded-full dark:border-dark-white-50 border font-medium bg-zinc-50"
-                  >
-                    {skill}
-                  </p>
-                ))}
-
-                <Link
-                  href="/skills"
-                  className="transition ease-in-out duration-300 dark:bg-dark-white-100 hover:bg-highlight-100 hover:border-highlight hover:text-highlight font-medium py-0.5 text-sm px-3 rounded-full dark:border-dark-white-50 border flex gap-1 group bg-zinc-50"
+        <h3 className="mb-5 text-lg">Skills</h3>
+        <div className="grid grid-cols-4 gap-y-4">
+          <p className="text-zinc-500 dark:text-dark-white-300">Tech Stack</p>
+          <div className="col-span-3">
+            <div className="flex gap-2 flex-wrap">
+              {skills.map((skill) => (
+                <p
+                  key={skill}
+                  className="dark:bg-dark-white-100 py-0.5 text-sm px-3 rounded-full dark:border-dark-white-50 border font-medium bg-zinc-50"
                 >
-                  See all
-                  <Icon
-                    name="social-link"
-                    className="w-1.5 h-1.5 group-hover:scale-125 mt-1.5"
-                  />
-                </Link>
-              </div>
+                  {skill}
+                </p>
+              ))}
+
+              <Link
+                href="/skills"
+                className="transition ease-in-out duration-300 dark:bg-dark-white-100 hover:bg-highlight-100 hover:border-highlight hover:text-highlight font-medium py-0.5 text-sm px-3 rounded-full dark:border-dark-white-50 border flex gap-1 group bg-zinc-50"
+              >
+                See all
+                <Icon
+                  name="social-link"
+                  className="w-1.5 h-1.5 group-hover:scale-125 mt-1.5"
+                />
+              </Link>
             </div>
           </div>
-        </div>
-        <div className="mt-8">
-          <h3 className="mb-5 text-lg">Education</h3>
-          {education.map((ed) => (
-            <div key={ed.end} className="grid grid-cols-4 gap-y-4 ">
-              <p className="text-zinc-500 dark:text-dark-white-300">
-                {ed.start} - {ed.end}
-              </p>
-              <div className="col-span-3">
-                <p>{ed.degree}</p>
-                <p className="text-zinc-500 dark:text-dark-white-300 tracking-wider mt-1.5 text-pretty">
-                  {ed.school}
-                </p>
-              </div>
-            </div>
-          ))}
         </div>
       </div>
-    </main>
-  );
-};
+      <div className="mt-8">
+        <h3 className="mb-5 text-lg">Education</h3>
+        {education.map((ed) => (
+          <div key={ed.end} className="grid grid-cols-4 gap-y-4 ">
+            <p className="text-zinc-500 dark:text-dark-white-300">
+              {ed.start} - {ed.end}
+            </p>
+            <div className="col-span-3">
+              <p>{ed.degree}</p>
+              <p className="text-zinc-500 dark:text-dark-white-300 tracking-wider mt-1.5 text-pretty">
+                {ed.school}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  </main>
+);
+
 export default Page;
